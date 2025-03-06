3384c0a738b2df1e2c0ae9082f2d9b010f52f4e1
Deepesh Bathija <deepeshbathija@ibm.com>
2025-03-06 02:15:55 +0000

    Add topics implementation
    Signed-off-by: Deepesh Bathija <deepeshbathija@gmail.com>

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
-
+	"github.com/IBM/openkommander/pkg/api"
 	"github.com/IBM/openkommander/pkg/commands"
 	"github.com/spf13/cobra"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -22,5 +28,27 @@ Complete documentation is available at https://github.com/IBM/openkommander`,
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+	router := api.NewRouter()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+		<-sigCh
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Error during server shutdown: %v", err)
+		}
+	}()
+
+	log.Println("REST API server running on port 8080...")
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("Server error: %v", err)
 	}
 }
